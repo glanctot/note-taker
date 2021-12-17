a87fba78391b95a9f04e3d26d6dbc203581dd4b2
glanctot <gregory.lanctot28@gmail.com>
2021-12-17 12:43:37 -0600

    first push

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001`);
+})
+
+//db.json file on back end to store and retrieve notes using fs module
+// HTML routes should be created
+    // GET /notes should return the notes.html
+    // GET * (wild card) should return index.html
+// API routes should be created
+    // GET /api/notes should read db.json and return all saved notes as JSON
+    // POST /api/notes should receive new note, add to db.json and return new note to client
+    // each note will need a unique id when saved (look into npm packes)
