5d0afc8c417d83e0d6303266e75d465de1c6fd8a
glanctot <gregory.lanctot28@gmail.com>
2021-12-20 11:29:11 -0600

    working mvp

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,20 +1,34 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-// const createNewNote = require('../../lib/notes.js');
-// const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../db/db.json'));
 })
 
-/* router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+router.post('/notes', (req, res) => {
+    const { title, text} = req.body;
 
-    const note = createNewNote(req.body, notes);
+    const newNote = {
+        title,
+        text,
+        id: uuidv4()
+    }
 
-    res.json(note);
-}); */
+
+    fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf-8', (err, data) => {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(parsedNotes), (err) => {
+            if(err) throw err;
+        })
+    });
+    res.sendFile(path.join(__dirname, '../../db/db.json'));
+});
+
+
 
 module.exports = router;
