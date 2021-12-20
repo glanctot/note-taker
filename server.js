2bec18d9958a41874fa4882d1d3a354bdae29164
glanctot <gregory.lanctot28@gmail.com>
2021-12-19 21:09:41 -0600

    push up

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+// const { uuid } = require('uuidv4');
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 const notes = require('./db/db.json');
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+function createNewNote(body, noteArray) {
+    const note = body;
+    noteArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: noteArray }, null, 2)
+    )
+
+    return note;
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+});
 
 
 
-
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 })
 
 //db.json file on back end to store and retrieve notes using fs module
