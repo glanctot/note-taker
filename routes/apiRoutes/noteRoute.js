15825e1d78c365fb47fdc78b631a6fa686596134
glanctot <gregory.lanctot28@gmail.com>
2021-12-20 10:13:05 -0600

    help push

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
-const createNewNote = require('../../lib/notes.js');
-const { notes } = require('../../db/db.json');
+const path = require('path');
+const fs = require('fs');
 
+// const createNewNote = require('../../lib/notes.js');
+// const { notes } = require('../../db/db.json');
 
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../db/db.json'));
+})
 
-router.post('/notes', (req, res) => {
+/* router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
 
     const note = createNewNote(req.body, notes);
 
     res.json(note);
-});
+}); */
 
 module.exports = router;
