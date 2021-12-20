4b53d01d39d7eb68293850c6f89038ddb5e459d0
glanctot <gregory.lanctot28@gmail.com>
2021-12-20 09:32:31 -0600

    working routers

--- server.js
+++ server.js
@@ -5,43 +5,17 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
+const apiRoutes = require('./routes/apiRoutes/noteRoute.js');
+app.use('/api', apiRoutes);
 
-const notes = require('./db/db.json');
-
-function createNewNote(body, noteArray) {
-    const note = body;
-    noteArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: noteArray }, null, 2)
-    )
-
-    return note;
-}
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
-
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-})
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    const note = createNewNote(req.body, notes);
-
-    res.json(note);
-});
-
-
+const htmlRoutes = require('./routes/htmlRoute/index.js');
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
